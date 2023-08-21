17acbf164ed4c95b429edeefd8b6af01d4e6d0df
Michael Chukwu <macdevis26@gmail.com>
2023-08-21 10:33:35 +0200

    Updated Nav.js

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -32,36 +32,44 @@ export default function Nav() {
     setShowMenu((menu) => !menu);
   }
   return (
-    <nav className="flex justify-between pb-3 col-[1_/_4] row-[1_/_2] pt-3">
-      <img src={logo} alt={"companies logo"} />
-      <img
-        className="md:hidden w-[3.75rem] h-[1.875rem]"
-        src={menu}
-        alt={"hamburger menu"}
-        onClick={menuHandler}
-      />
-      <div className=" hidden gap-10 md:flex">
-        {navItem.map((nav) => (
-          <NavItems nav={nav} key={nav.id} />
-        ))}
-      </div>
+    <>
       {showMenu && (
-        <div className="absolute bg-offWhite  min-h-screen w-[64%] right-0">
-          <div className="relative">
-            <img
-              className="md:hidden absolute right-7 top-3"
-              src={menuClose}
-              alt={"X diagram to close the menu"}
-              onClick={menuHandler}
-            />
-            <div className="space-y-6 p-6 pt-28 text-xl">
-              {navItem.map((nav) => (
-                <NavItems nav={nav} key={nav.id} />
-              ))}
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50"
+          onClick={menuHandler}
+        ></div>
+      )}
+      <nav className="flex justify-between pb-3 col-[1_/_4] row-[1_/_2] pt-3">
+        <img src={logo} alt={"companies logo"} />
+        <img
+          className="md:hidden w-[3.75rem] h-[1.875rem]"
+          src={menu}
+          alt={"hamburger menu"}
+          onClick={menuHandler}
+        />
+        <div className=" hidden gap-10 md:flex">
+          {navItem.map((nav) => (
+            <NavItems nav={nav} key={nav.id} />
+          ))}
+        </div>
+        {showMenu && (
+          <div className="fixed top-0 bg-offWhite  min-h-screen w-[64%] right-0">
+            <div className="relative">
+              <img
+                className="md:hidden absolute right-7 top-3"
+                src={menuClose}
+                alt={"X diagram to close the menu"}
+                onClick={menuHandler}
+              />
+              <div className="space-y-6 p-6 pt-28 text-xl">
+                {navItem.map((nav) => (
+                  <NavItems nav={nav} key={nav.id} />
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-      )}
-    </nav>
+        )}
+      </nav>
+    </>
   );
 }
